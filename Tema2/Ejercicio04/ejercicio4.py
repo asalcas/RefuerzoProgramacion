488dbfa380070410d74ee070d19e8997d1942055
BaiHuu <alvaro.salvador@iesnervion.es>
2024-11-18 14:35:23 +0100

    hasta ejercicio4

---
+++ Tema2/Ejercicio04/ejercicio4.py
@@ -0,0 +1,10 @@
+#coding: latin1
+
+class ejercicio4:
+    num1 = input ("Dame un numero: ")
+    num2 = input ("Dame un segundo numero: ")
+
+    if (num1 > num2):
+        print(f"{num1} \n{num2}")
+    elif (num1 < num2):
+        print (f"{num2} \n{num1}")
--- Tema2/Ejercicio05/ejercicio5.java
+++ Tema2/Ejercicio05/ejercicio5.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class ejercicio5 {
+    public static void main(String[] args) {
+        
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Introduce un numero: ");
+        int num1 = lector.nextInt();
+        System.out.println("Introduce otro numero: ");
+        int num2 = lector.nextInt();
+        System.out.println("Introduce un ultimo numero: ");
+        int num3 = lector.nextInt();
+
+        if ( num1 > num2 && num2 > num3){
+            System.out.println("num1 > num2 > num3");
+        }else if (num2 > num1 && num1 > num3) {
+            System.out.println("num2 > num1 > num3");
+        }else if (num3 > num2 && num2 > num1){
+            System.out.println("num3 > num2 > num1");
+        }else if (num1 > num3 && num3 > num2){
+            System.out.println("num1 > num3 > num2");
+        }else if (num2 > num3 && num3 > num1){
+            System.out.println("num2 > num3 > num1");
+        }
+    }
+
+    //! HACERLO CON IF ANIDADOS
 
 }
