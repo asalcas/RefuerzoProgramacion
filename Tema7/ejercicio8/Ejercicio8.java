8a0ecfa614f54f78e399c0cb5152367d282a8af0
BaiHuu <alvaro.salvador@iesnervion.es>
2024-12-26 16:28:19 +0100

    ahora faltan solo los ejercicios complicados

--- Tema7/ejercicio8/Ejercicio8.java
+++ Tema7/ejercicio8/Ejercicio8.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Ejercicio8 {
     public static void main(String[] args) {
+        System.out.println("Escribe dos numeros y te diremos los que estén entre ambos :)");
         Scanner lector = new Scanner(System.in);
         int a = lector.nextInt();
         int b = lector.nextInt();
@@ -14,7 +15,7 @@ public class Ejercicio8 {
             b = a;
             a = temporal;
         }
-        System.out.println("Los numeros entre los que tu dijiste ("+ a +"," + b +") son: ");
+        System.out.println("Los numeros entre los que tu dijiste ("+ a +", " + b +") son: ");
         for (int i = a; a <= b; a++) { // SEGURAMENTE TENGA ALGO QUE MEJORAR AQUI, intenté poner el valor de a como 1 
             // elemento y no funcionó, y forcé que funcionara metiendole una variable que guarde "a" para que funcione.
             System.out.print(a + " ");
