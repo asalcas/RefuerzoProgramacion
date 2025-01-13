faede7f73a94c312908f9161be0c5683fbd57b1c
BaiHuu <alvaro.salvador@iesnervion.es>
2025-01-13 14:35:53 +0100

    arrays completado

--- Tema8Arrays/ejercicio8/Ejercicio8Arrays.java
+++ Tema8Arrays/ejercicio8/Ejercicio8Arrays.java
@@ -1,7 +1,34 @@
 package ejercicio8;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ejercicio8Arrays {
     public static void main(String[] args) {
+        Random rd = new Random();
+        Scanner lector = new Scanner(System.in);
+        int[] array100 = new int[100];
+        int numeroUsuario;
+        int apariciones = 0;
+
+        String cadenaArray= "";
         
+        for (int i = 0; i < array100.length; i++){
+        array100[i] =  rd.nextInt(1,11);
+        }
+        cadenaArray = Arrays.toString(array100);
+        System.out.println(cadenaArray);
+        System.out.print("Dime un numero para mostrarte en que posiciones aparece: ");
+        numeroUsuario = lector.nextInt();
+
+        for (int i = 0; i < array100.length; i++){
+            if(numeroUsuario == array100[i]){
+                System.out.println(String.format("El numero aparece en la posición: %d", i));
+                apariciones++;
+            }
+        }
+        System.out.println(String.format("En total aparece: %d veces.", apariciones));
+        lector.close();
     }
 }
