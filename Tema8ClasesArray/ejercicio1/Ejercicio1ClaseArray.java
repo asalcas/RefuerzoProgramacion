466a5d216a99d3378d88483103d9f567044ed33c
BaiHuu <alvaro.salvador@iesnervion.es>
2025-01-14 14:38:10 +0100

    no me dio tiempo a empezar

---
+++ Tema8ClasesArray/ejercicio1/Ejercicio1ClaseArray.java
@@ -0,0 +1,11 @@
+package ejercicio1;
+
+import java.util.Scanner;
+
+public class Ejercicio1ClaseArray {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+
+    }
+
+}
