15b2c091bd87082e58d17460bca840afbd1582e8
BaiHuu <alvaro.salvador@iesnervion.es>
2025-01-21 11:53:21 +0100

    Ejercicio 5 bien hecho

--- Tema8ClasesArray/ejercicio5/Ejercicio5ClaseArray.java
+++ Tema8ClasesArray/ejercicio5/Ejercicio5ClaseArray.java
@@ -6,41 +6,34 @@ import java.util.Scanner;
 
 public class Ejercicio5ClaseArray {
     public static void main(String[] args) {
-        int[] arrayLargo = new int[10];
+        int[] arrayLargo = new int[1000];
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
-        int numeroUsuario;
+        int numeroUsuario = 0;
         int contadorN = 0;
         String cadenaArray = "";
-        int[] arrayOrdenado;
-
+        
 
         for(int i = 0; i < arrayLargo.length; i++){
             arrayLargo[i] = rd.nextInt(0,99);
         }
         cadenaArray = Arrays.toString(arrayLargo);
         System.out.println(cadenaArray);
+
+        System.out.println("Escribe un numero ver cuantas veces aparece en el array: ");
         numeroUsuario = sc.nextInt();
 
-        int cosa;
-        
-        /* Nada, otra vez se me ha olvidado usar la funcion, tontisimo 
-        
         for(int i = 0; i< arrayLargo.length; i++){
             if(numeroUsuario == arrayLargo[i]){
                 contadorN++;
             }
         }
         if(contadorN > 0){
-            System.out.println(String.format("Si existe, y aparece %d veces", contadorN));
+            System.out.println(String.format("Si existe en el array, y aparece %d veces", contadorN));
         }else{
             System.out.println("No, no aparece en la lista");
         }
-        */
-        Arrays.sort(arrayLargo);
-        cosa = Arrays.binarySearch(arrayLargo, numeroUsuario);
         
-        System.out.println(cosa);
         sc.close();
     }
 }
