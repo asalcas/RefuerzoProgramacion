910603c38f8f1345ea03f499a67c9360e22f6e96
BaiHuu <alvaro.salvador@iesnervion.es>
2025-01-23 00:38:57 +0100

    piensa

--- Tema8ClasesArray/ejercicio7/Ejercicio7ClaseArray.java
+++ Tema8ClasesArray/ejercicio7/Ejercicio7ClaseArray.java
@@ -9,8 +9,15 @@ public class Ejercicio7ClaseArray {
         for(int i = 0; i < arrayNumeros.length; i++){
         // Desde
         // Hasta
-        // fill (i) desde () hasta ()
+        // fill (i) desde (suma de todos los numeros previos a i) hasta (desde + i)
+        // contador = i(o algo asi)
+        // while (contador != 0){
+        // variable a contar += array[i]
+        // contador--
         
+        // }
+
+        // 1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7,7
         }
     }
 }
