faab9a846919d632cbc99b54fb46007c2e6cbeae
BaiHuu <alvaro.salvador@iesnervion.es>
2025-01-29 14:38:06 +0100

    tienes que terminar hoy los ejercicios de bidimiensionales

---
+++ Tema8ArraysBidimensionales/ejercicio4/Ejercicio4Bidimensionales.java
@@ -0,0 +1,10 @@
+package ejercicio4;
+
+public class Ejercicio4Bidimensionales {
+    public static void main(String[] args) {
+        //Escribe un programa que cree una tabla de 10x10 que contenga los valores de las tablas de multiplicar del 1 al 10 
+        //(cada tabla de multiplicar en una fila). Muestra la tabla por pantalla.
+
+
+    }
+}
