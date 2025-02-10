fcbbbdd19766c2b7e1f30faf8b376a8d748ec5a3
BaiHuu <alvaro.salvador@iesnervion.es>
2025-02-10 14:39:24 +0100

    hacer el 5 y 6

--- Tema8Funciones/ejercicio4/Ejercicio4Funciones.java
+++ Tema8Funciones/ejercicio4/Ejercicio4Funciones.java
@@ -1,49 +1,50 @@
 package ejercicio4;
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Ejercicio4Funciones {
     public static void main(String[] args) {
-        int[] t = new int[19];
-        Scanner sc = new Scanner(System.in);
-        int claveUsuarioBusqueda;
-        int coincide ;
+        int[] t = new int[10];
+        try (Scanner sc = new Scanner(System.in)) {
+            int claveUsuarioBusqueda;
+            int coincide;
 
-        llenarArray(t);
-        System.out.println("Intoduce un numero para buscarlo en la lista: (1-10)");
-        claveUsuarioBusqueda = sc.nextInt();
-        coincide = buscar(t, claveUsuarioBusqueda);
-        if (coincide == -1){
-            System.out.println("No está en la lista");
-        }else{
-            System.out.println("Está, y su posicion es: " + coincide);
+            llenarArray(t);
+            System.out.println("Intoduce un numero para buscarlo en la lista: (1-10)");
+            claveUsuarioBusqueda = sc.nextInt();
+            coincide = buscar(t, claveUsuarioBusqueda);
+            if (coincide == -1) {
+                System.out.println("No está en la lista");
+            } else {
+                System.out.println("Está, y su posicion es: " + coincide);
+            }
         }
-        
+
     }
 
-    public static int buscar(int[] t, int clave){
-        int encontrado = 0;
-        for(int i = 0; i < t.length;i++){
-            if( clave == t[i]){
+    public static int buscar(int[] t, int clave) {
+        boolean haSidoEncontrado = false;
+        int encontrado = -1;
+        int i = 0;
 
-
-                encontrado = i; 
-                
-            }else{
-                encontrado = -1; // ESTA MACHACANDO TODO EL RATO con -1 el resultado
+        while (i < t.length && !haSidoEncontrado) {
+            if (clave == t[i]) {
+                encontrado = i;
+                haSidoEncontrado = true;
             }
-
+            i++;
         }
-        System.out.println(Arrays.toString(t));
+
+        // System.out.println(Arrays.toString(t)); Prueba para ver todo el Array
         return encontrado;
     }
-    public static void llenarArray(int[] t){
-        
+
+    public static void llenarArray(int[] t) {
+
         Random rd = new Random();
-        for(int i = 0; i < t.length; i++){
-            t[i] = rd.nextInt(1,10);
+        for (int i = 0; i < t.length; i++) {
+            t[i] = rd.nextInt(1, 10);
         }
     }
 }
---
+++ Tema8Funciones/ejercicio5/Ejercicio5Funciones.java
@@ -0,0 +1,59 @@
+package ejercicio5;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio5Funciones {
+    public static void main(String[] args) {
+        Random numeroAleatorio = new Random();
+
+        try (Scanner sc = new Scanner(System.in)) {
+            int[] arrayDondeBuscarIndices = new int[10];
+            for(int i = 0; i < arrayDondeBuscarIndices.length; i++){
+                arrayDondeBuscarIndices[i] = numeroAleatorio.nextInt(1,11);
+            }
+            System.out.println("Escribe un numero del (1-10) un numero para ver si se encuentra en la lista: ");
+            int numeroBuscar = sc.nextInt();
+
+            int[] indices = buscarTodos(arrayDondeBuscarIndices, numeroBuscar);
+            System.out.println(Arrays.toString(arrayDondeBuscarIndices));
+            System.out.println(Arrays.toString(indices));
+        }
+
+    }
+
+    public static int[] buscarTodos(int[] array, int clave) {
+        int contador = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (clave == array[i]) {
+                contador++;
+            }
+            
+        }
+        
+        int[] arrayIndices = new int[contador];
+        contador = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(clave == array[i]){
+                arrayIndices[contador] = i;
+                contador++;
+            }
+        }
+        return arrayIndices;
+        
+    }
+}
+/*
+ * Realiza la función: int[] buscarTodos(int t[], int valor),
+ * que crea y devuelve una tabla con todos los índices de los elementos donde se
+ * encuentra el valor de búsqueda.
+ * En el caso de que el valor no se encuentre en la tabla t, la función
+ * devolverá una tabla vacía.
+ * 
+ */
+
+ // REHAZLO AQUI
+
+ 
