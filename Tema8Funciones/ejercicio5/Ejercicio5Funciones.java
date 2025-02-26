6a37c801f84ae04c0e6c38f378996ab5966197dd
BaiHuu <alvaro.salvador@iesnervion.es>
2025-02-26 14:33:15 +0100

    comentarios de act 5

--- Tema8Funciones/ejercicio5/Ejercicio5Funciones.java
+++ Tema8Funciones/ejercicio5/Ejercicio5Funciones.java
@@ -10,8 +10,8 @@ public class Ejercicio5Funciones {
 
         try (Scanner sc = new Scanner(System.in)) {
             int[] arrayDondeBuscarIndices = new int[10];
-            for(int i = 0; i < arrayDondeBuscarIndices.length; i++){
-                arrayDondeBuscarIndices[i] = numeroAleatorio.nextInt(1,11);
+            for (int i = 0; i < arrayDondeBuscarIndices.length; i++) {
+                arrayDondeBuscarIndices[i] = numeroAleatorio.nextInt(1, 11);
             }
             System.out.println("Escribe un numero del (1-10) un numero para ver si se encuentra en la lista: ");
             int numeroBuscar = sc.nextInt();
@@ -23,6 +23,17 @@ public class Ejercicio5Funciones {
 
     }
 
+    /*
+     * Realiza la función: int[] buscarTodos(int t[], int valor),
+     * que crea y devuelve una tabla con todos los índices de los elementos donde se
+     * encuentra el valor de búsqueda.
+     * En el caso de que el valor no se encuentre en la tabla t, la función
+     * devolverá una tabla vacía.
+     * 
+     */
+
+    // REHAZLO AQUI
+
     public static int[] buscarTodos(int[] array, int clave) {
         int contador = 0;
 
@@ -30,30 +41,21 @@ public class Ejercicio5Funciones {
             if (clave == array[i]) {
                 contador++;
             }
-            
+
         }
-        
-        int[] arrayIndices = new int[contador];
-        contador = 0;
-        for (int i = 0; i < array.length; i++) {
-            if(clave == array[i]){
-                arrayIndices[contador] = i;
-                contador++;
+
+        int[] arrayIndices = new int[contador]; // Creamos un array con la longitud de contador (si apareció 3 veces el
+                                                // array sera de 3 casillas)
+        contador = 0; // Lo machacamos a 0
+        for (int i = 0; i < array.length; i++) { // Recorremos el array
+            if (clave == array[i]) { // si coincide
+                arrayIndices[contador] = i; // entonces a la posicion de 'arrayIndices' en el valor contado (si hay 3
+                                            // [0,1,2]) en este caso es 0
+                contador++;// y lo incrementamos
             }
         }
         return arrayIndices;
-        
+
     }
+
 }
-/*
- * Realiza la función: int[] buscarTodos(int t[], int valor),
- * que crea y devuelve una tabla con todos los índices de los elementos donde se
- * encuentra el valor de búsqueda.
- * En el caso de que el valor no se encuentre en la tabla t, la función
- * devolverá una tabla vacía.
- * 
- */
-
- // REHAZLO AQUI
-
- 
